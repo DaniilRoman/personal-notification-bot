6b03d928d71868cf33352637a28ee6661fc252ea
Daniil Roman <danroman17397@gmail.com>
2022-09-29 22:28:08 +0200

    Add Hertha tickets

---
+++ modules/herthaTickets.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+def hertha_tickets():
+    res = ""
+    url = "https://www.ticket-onlineshop.com/ols/hbsctk/en/tk/channel/shop/index"
+
+    return res
+
+
+if __name__ == '__main__':
+    print(hertha_tickets())
