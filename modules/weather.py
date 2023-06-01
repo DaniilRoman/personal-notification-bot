3fd931cf7811455aa4c238fb54a31c6a16dc736b
Daniil Roman <danroman17397@gmail.com>
2023-06-01 14:14:11 +0200

    Update weather.py

--- modules/weather.py
+++ modules/weather.py
@@ -19,15 +19,15 @@ def _weather(token):
     city_name = weather["city"]["name"]
     sunset_time = _timestamp_to_date(weather["city"]["sunset"])
 
-    temp_now = weather["list"][0]["main"]["temp"]
-    temp_afternoon = weather["list"][2]["main"]["temp"]  # +6 hours
-    temp_evening = weather["list"][4]["main"]["temp"]  # +12 hours
+    temp_now = weather["list"][1]["main"]["temp"] # +3 hours ~10:00
+    temp_afternoon = weather["list"][2]["main"]["temp"]  # +6 hours ~13:00
+    temp_evening = weather["list"][4]["main"]["temp"]  # +12 hours ~19:00
 
     temp_str = f"{temp_now} :: {temp_afternoon} :: {temp_evening}"
 
     precipitation = " - ".join(set(w["main"] for entry in weather["list"] for w in entry["weather"]))
 
-    return f"Today in {city_name} temp: {temp_str}. \nWeather: {precipitation}. \nSunset at {sunset_time}"
+    return f"Today in {city_name}: {temp_str} \nWeather: {precipitation} \nSunset at {sunset_time}"
 
 
 def weather(token):
