0f2dc9995b3676e74701ce63a20944e91fa041e3
Daniil Roman <danroman17397@gmail.com>
2023-12-17 14:24:39 +0100

    revert model from gpt4 to gpt3.5

--- app/utils/chatgptSummarizing.go
+++ app/utils/chatgptSummarizing.go
@@ -18,7 +18,7 @@ func SummarizeText(text string, client *openai.Client) string {
         text = text[:4000]
     }
 	inputText := "Give me the main ideas of this tech article: " + text
-	model := openai.GPT4
+	model := openai.GPT3Dot5Turbo
 	maxTokens := 300
 
 	request := openai.ChatCompletionRequest{
