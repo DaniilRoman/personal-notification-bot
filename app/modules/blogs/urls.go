f22832654fea6ddf363c66c9d7cea1bc19fe308c
Daniil Roman <danroman17397@gmail.com>
2024-03-04 00:16:23 +0100

    refactoring of the blog urls

--- app/modules/blogs/urls.go
+++ app/modules/blogs/urls.go
@@ -1,25 +1,20 @@
 package blogs
 
-
-var blackListKeywords = []string {
-	"android", "ios", "redux", "react", "frontend", 
+var blackListKeywords = []string{
+	"android", "ios", "redux", "react", "frontend",
 	"ui/ux", "career stories", "meeting", "spotlight",
-    "internship", "javascript", "css", "html", "typescript", 
-	"mobile", "uikit", "интерфейс", "дизайн", "мобильн", 
+	"internship", "javascript", "css", "html", "typescript",
+	"mobile", "uikit", "интерфейс", "дизайн", "мобильн",
 	"design", "interface", "A Bootiful Podcast", "Spark", "Docker Desktop",
 }
 
-var blogsUrls = []string {
+var blogsUrls = []string{
 	// Personal blogs
 	"https://vladmihalcea.com/blog/feed/",
 	"https://piotrminkowski.com/feed/",
 	"https://blog.alexellis.io/rss/",
 
 	// Interesting company blogs
-	"https://feeds.feedblitz.com/baeldung&x=1",
-	"https://spring.io/blog.atom",
-	"https://blog.jetbrains.com/kotlin/category/server/feed/",
-	"https://aws.amazon.com/blogs/aws/feed/",
 	"https://medium.com/feed/netcracker",
 	"https://habr.com/ru/rss/company/just_ai/blog/?fl=ru",
 	"https://medium.com/feed/adevinta-tech-blog",
@@ -38,7 +33,7 @@ var blogsUrls = []string {
 
 	// Research labs
 	// https://githubnext.com, # didn't find rss feed,
-	"http://feeds.feedburner.com/blogspot/gJZg",  // Google research
+	"http://feeds.feedburner.com/blogspot/gJZg", // Google research
 	"https://openai.com/blog/rss.xml",
 	"https://research.facebook.com/feed/",
 	"https://www.microsoft.com/en-us/research/feed/",
@@ -46,24 +41,20 @@ var blogsUrls = []string {
 	"https://www.amazon.science/index.rss",
 
 	// Tech
-	"https://circleci.com/blog/feed.xml",
-	"https://kubernetes.io/feed.xml",
-	"https://www.docker.com/blog/feed",
+	"https://www.docker.com/blog/category/engineering/feed/",
 	"https://redis.com/blog/rss",
-	"https://www.mongodb.com/blog/rss",
-	"https://debezium.io/blog.atom",
-	"https://www.elastic.co/blog/feed",
 	"https://aws.amazon.com/blogs/database/tag/dynamodb/feed",
 	"https://about.gitlab.com/atom.xml",
-	"https://feeds.feedburner.com/ContinuousBlog/",  // Jenkins
-	"https://in.relation.to/blog.atom",  // Hibernate
+	"https://feeds.feedburner.com/ContinuousBlog/", // Jenkins
 	"https://www.cncf.io/blog/feed/",
 
 	// Others company bogs
-	"https://www.hashicorp.com/blog/feed.xml",
+	"https://aws.amazon.com/ru/blogs/machine-learning/feed/",
+	"https://aws.amazon.com/ru/blogs/architecture/feed/",
+	"https://aws.amazon.com/ru/blogs/database/feed/",
 	"https://microservices.io/feed.xml",
-	"https://www.confluent.io/rss.xml",
-	"https://blog.cloudflare.com/rss",
+	"https://www.confluent.io/blog/area/technology/rss.xml",
+	"https://blog.cloudflare.com/tag/developers",
 	// "https://www.uber.com/en-DE/blog/engineering/rss", // TODO http error: 406 Not Acceptable
 	"https://medium.com/feed/miro-engineering",
 	"https://habr.com/ru/rss/company/ozontech/blog/?fl=ru",
@@ -81,6 +72,17 @@ var blogsUrls = []string {
 	// Company blogs to delete
 	"https://engineering.fb.com/feed/",
 	// "https://blog.twitter.com/engineering/en_us/blog.rss", // TODO no published date
-	"https://www.datadoghq.com/blog/index.xml",
+	"https://www.datadoghq.com/blog/engineering/index.xml",
 	"https://grafana.com/categories/engineering/index.xml",
+
+	// "https://feeds.feedblitz.com/baeldung&x=1",
+	// "https://spring.io/blog.atom",
+	// "https://www.mongodb.com/blog/rss",
+	"https://blog.jetbrains.com/kotlin/category/server/feed/",
+	"https://circleci.com/blog/feed.xml",
+	"https://kubernetes.io/feed.xml",
+	"https://debezium.io/blog.atom",
+	"https://www.elastic.co/blog/feed",
+	"https://in.relation.to/blog.atom", // Hibernate
+	"https://www.hashicorp.com/blog/categories/products-technology/feed.xml",
 }
