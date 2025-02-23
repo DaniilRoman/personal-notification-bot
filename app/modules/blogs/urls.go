3b2ddda74aa0355a0a0b90fdeac467af8848e752
Daniil Roman <danroman17397@gmail.com>
2025-02-23 21:06:52 +0100

    Update urls.go

--- app/modules/blogs/urls.go
+++ app/modules/blogs/urls.go
@@ -10,6 +10,7 @@ var blackListKeywords = []string{
 
 var blogsUrls = []string{
 	// Personal blogs
+	"https://valeriansaliou.name/blog/rss/",
 	"https://jazco.dev/atom.xml",
 	"https://vas3k.blog/rss/",
 	"https://vladmihalcea.com/blog/feed/",
