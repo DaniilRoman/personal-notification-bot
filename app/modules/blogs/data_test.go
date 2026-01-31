afa65e2a8ab9a56a81cca01a736ee7c936ff390e
Daniil Roman <danroman17397@gmail.com>
2026-01-31 22:48:49 +0100

    [blogs] update data_test

--- app/modules/blogs/data_test.go
+++ app/modules/blogs/data_test.go
@@ -22,3 +22,32 @@ func Test_websiteName_habr(t *testing.T) {
 
 	assert.Equal(t, "ozontech", actualUrl)
 }
+
+func Test_GetUpdateStrings(t *testing.T) {
+	// Test with nil BlogUpdateData
+	var nilData *BlogUpdateData
+	assert.Empty(t, nilData.GetUpdateStrings())
+	
+	// Test with empty BlogUpdateData
+	emptyData := &BlogUpdateData{Blogs: []blogUpdate{}}
+	assert.Empty(t, emptyData.GetUpdateStrings())
+	
+	// Test with blog updates
+	blogData := &BlogUpdateData{
+		Blogs: []blogUpdate{
+			{
+				Title: "Test Blog 1",
+				Link:  "https://example.com/blog1",
+			},
+			{
+				Title: "Test Blog 2",
+				Link:  "https://medium.com/test/blog2",
+			},
+		},
+	}
+	
+	updateStrings := blogData.GetUpdateStrings()
+	assert.Len(t, updateStrings, 2)
+	assert.Equal(t, "- Test Blog 1 {[example.com](https://example.com/blog1)}", updateStrings[0])
+	assert.Equal(t, "- Test Blog 2 {[test](https://medium.com/test/blog2)}", updateStrings[1])
+}
